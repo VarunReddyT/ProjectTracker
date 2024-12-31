d862053bed8e7a3efcd9689866bad69954accb51
VarunReddyT <tvarun2014@gmail.com>
2024-12-31 21:22:36 +0530

    Project

--- lib/screens/viewprojects.dart
+++ lib/screens/viewprojects.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class Viewprojects extends StatefulWidget {
   const Viewprojects({super.key});
@@ -8,8 +10,33 @@ class Viewprojects extends StatefulWidget {
 }
 
 class _ViewprojectsState extends State<Viewprojects> {
+
+  @override 
+  void initState(){
+    super.initState();
+    fetchProjects();
+  }
+  void fetchProjects() async{
+    try{
+      var response = await http.get(Uri.parse('https://ps-project-tracker.vercel.app/api/project/getProjects'));
+    }
+    catch(e){
+      if(mounted){
+        ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Failed to fetch projects: $e')));
+      }
+    }
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Projects"),
+        centerTitle: true,
+      ),
+      // body: SingleChildScrollView(
+      //   child: ,
+      // )
+    );
   }
 }
